4531ac27f002f34a231d6d23b737877d34b31263
Kovalenko <amidalo148@gmail.com>
2025-07-15 17:07:12 +0500

    add findByName(String projectName) method

--- src/main/java/wild/yellow/tasktrackerapi/store/repositories/ProjectRepository.java
+++ src/main/java/wild/yellow/tasktrackerapi/store/repositories/ProjectRepository.java
@@ -3,5 +3,9 @@ package wild.yellow.tasktrackerapi.store.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wild.yellow.tasktrackerapi.store.entities.ProjectEntity;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+
+    Optional<ProjectEntity> findByName(String projectName);
 }
